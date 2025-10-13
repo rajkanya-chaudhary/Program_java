bcaddd27a4a08180627ecdf0a7e11eeec5b29c91
rajkanya-chaudhary <dc5068786@gmail.com>
2025-10-13 18:51:49 +0530

    this is a simple program using java

---
+++ Simple_code.java
@@ -0,0 +1,7 @@
+ public class Simple_code {
+   public static void main(String[] args) {
+    System.out.println("dimpal chuadhary");
+    System.out.println("Print all String Question ");
+   }
+    
+ }
